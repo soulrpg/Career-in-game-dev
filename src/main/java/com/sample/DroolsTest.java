10c0b08a3f35f9126d98efef70badf162c165698
soulrpg <soulrpg@users.noreply.github.com>
2020-12-09 19:21:04 +0100

    Hello world

---
+++ src/main/java/com/sample/DroolsTest.java
@@ -0,0 +1,58 @@
+package com.sample;
+
+import org.kie.api.KieServices;
+import org.kie.api.runtime.KieContainer;
+import org.kie.api.runtime.KieSession;
+import org.kie.api.logger.*;
+public class DroolsTest {
+
+    public static final void main(String[] args) {
+        try {
+            // load up the knowledge base
+	        KieServices ks = KieServices.Factory.get();
+    	    KieContainer kContainer = ks.getKieClasspathContainer();
+        	KieSession kSession = kContainer.newKieSession("ksession-rules");
+        	KieRuntimeLogger kLogger = ks.getLoggers().newFileLogger(kSession, "test");
+
+            // go !
+            Message message = new Message();
+            message.setMessage("Hello World");
+            message.setStatus(Message.HELLO);
+            kSession.insert(message);
+            kSession.fireAllRules();
+            kLogger.close();
+        } catch (Throwable t) {
+            t.printStackTrace();
+        }
+    }
+
+    public static class Message {
+
+        public static final int HELLO = 0;
+        public static final int GOODBYE = 1;
+
+        private String message;
+
+        private int status;
+
+        public String getMessage() {
+            return this.message;
+        }
+
+        public void setMessage(String message) {
+            this.message = message;
+        }
+
+        public int getStatus() {
+            return this.status;
+        }
+
+        public void setStatus(int status) {
+            this.status = status;
+        }
+
+    }
+    
+	}
+
+}
