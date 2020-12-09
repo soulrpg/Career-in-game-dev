cc134cc84d9cf5d0ff2b2c5b0e284ff670c98e4e
soulrpg <soulrpg@users.noreply.github.com>
2020-12-09 22:06:07 +0100

    :<

--- src/main/java/com/sample/DroolsTest.java
+++ src/main/java/com/sample/DroolsTest.java
@@ -4,15 +4,47 @@ import org.kie.api.KieServices;
 import org.kie.api.runtime.KieContainer;
 import org.kie.api.runtime.KieSession;
 import org.kie.api.logger.*;
-public class DroolsTest {
+
+import javax.swing.*;
+import java.awt.*;
+
+public class DroolsTest{
 
     public static final void main(String[] args) {
+    	JFrame f = new JFrame();
+    	JButton b = new JButton("Zatwierdü");
+    	JRadioButton r1 = new JRadioButton("1");
+    	JRadioButton r2 = new JRadioButton("2");
+    	JRadioButton r3 = new JRadioButton("3");
+    	JLabel label = new JLabel("Pytanie");
+        r3.setVisible(false);
+        ButtonGroup bg = new ButtonGroup();
+        bg.add(r1);
+        bg.add(r2);
+        bg.add(r3);
+    	f.add(label);
+    	f.add(r1);
+    	f.add(r2);
+    	f.add(r3);
+    	f.add(b);
+    	b.setHorizontalAlignment(JButton.CENTER);
+    	f.setSize(300, 300);
+    	f.setLayout(new BoxLayout(f.getContentPane(), BoxLayout.Y_AXIS));
+    	f.setVisible(true);
+    	
         try {
             // load up the knowledge base
 	        KieServices ks = KieServices.Factory.get();
     	    KieContainer kContainer = ks.getKieClasspathContainer();
         	KieSession kSession = kContainer.newKieSession("ksession-rules");
         	KieRuntimeLogger kLogger = ks.getLoggers().newFileLogger(kSession, "test");
+        	
+        	kSession.setGlobal( "frame", f);
+        	kSession.setGlobal( "label", label);
+        	kSession.setGlobal( "r1", r1);
+        	kSession.setGlobal( "r2", r2);
+        	kSession.setGlobal( "r3", r3);
+        	kSession.setGlobal("button", b);
 
             // go !
             Message message = new Message();
@@ -53,6 +85,5 @@ public class DroolsTest {
 
     }
     
-	}
 
 }
